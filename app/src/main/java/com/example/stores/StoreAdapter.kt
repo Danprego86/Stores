9727765c00c3a71f004f8f987a7ac451ae482093
Danprego86 <danpreacu@gmail.com>
2023-06-14 10:08:43 -0500

    Se agrega Adapter

---
+++ app/src/main/java/com/example/stores/StoreAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.stores
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class StoreAdapter {
+
+    //Class Inner
+    inner class ViewHolder(view:View): RecyclerView.ViewHolder(view)
+}
